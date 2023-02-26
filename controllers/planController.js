11efeccb300e3a4b0620502f970dad6980c00563
Henry Steven Fuérez Morán <henry256@hotmail.es>
2023-02-25 22:24:20 -0500

    fix: remove unused

--- controllers/planController.js
+++ controllers/planController.js
@@ -3,12 +3,6 @@ const factory = require('./handlerFactory');
 
 exports.getAllPlans = factory.getAll(Plan);
 
-exports.setTourUserIds = (req, res, next) => {
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
-  next();
-};
-
 exports.getPlan = factory.getOne(Plan);
 
 exports.createPlan = factory.createOne(Plan);
